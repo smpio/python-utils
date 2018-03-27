4399579460b4fbd6afeff3e94d9b2824a8e351d7
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-03-27 20:51:25 +0300

    django: imrove celery command

--- utils/django/commands/celery.py
+++ utils/django/commands/celery.py
@@ -5,14 +5,11 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def run_from_argv(self, argv):
-        from celery import maybe_patch_concurrency
         from celery.bin.celery import main
+        argv = ['celery', '-A', self.get_celery_module_name()] + argv[2:]
+        main(argv)
 
+    def get_celery_module_name(self):
         settings_module = os.environ['DJANGO_SETTINGS_MODULE']
         project_module = settings_module.split('.', 1)[0]
-        argv = ['celery', '-A', project_module] + argv[2:]
-
-        if 'multi' not in argv:
-            maybe_patch_concurrency(argv)
-
-        main(argv)
+        return project_module + '.celery'
