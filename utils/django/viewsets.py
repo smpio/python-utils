1204260337036d626ec1f70778da4107216ec613
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-03-29 13:49:28 +0300

    add django viewsets

---
+++ utils/django/viewsets.py
@@ -0,0 +1,13 @@
+from rest_framework import mixins, viewsets
+
+
+class UndestroyableModelViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, and `list()` actions.
+    """
+    pass
