6c18e6a57f67402d27b306aad3badfa67d0d2fed
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-04-02 20:53:00 +0300

    add utils.django.serializers.mixins.WeiteOnceFieldsMixin

--- utils/django/serializers/mixins.py
+++ utils/django/serializers/mixins.py
@@ -1,4 +1,4 @@
-class WriteableFieldsMixin(object):
+class WriteableFieldsMixin:
     def get_extra_kwargs(self):
         extra_kwargs = super().get_extra_kwargs()
 
@@ -6,6 +6,20 @@ class WriteableFieldsMixin(object):
         writable_fields = getattr(self.Meta, 'writable_fields', [])
         for field_name in all_fields:
             if field_name not in writable_fields:
+                kwargs = extra_kwargs.get(field_name, {})
+                kwargs['read_only'] = True
+                extra_kwargs[field_name] = kwargs
+
+        return extra_kwargs
+
+
+class WriteOnceFieldsMixin:
+    def get_extra_kwargs(self):
+        extra_kwargs = super().get_extra_kwargs()
+
+        if self.context['request'].view.action != 'create':
+            write_once_fields = getattr(self.Meta, 'write_once_fields', [])
+            for field_name in write_once_fields:
                 kwargs = extra_kwargs.get(field_name, {})
                 kwargs['read_only'] = True
                 extra_kwargs[field_name] = kwargs
