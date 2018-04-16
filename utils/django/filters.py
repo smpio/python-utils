ca19b95935a4cdb6b6ab21e53d3e508b68273795
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-04-16 18:32:18 +0300

    add utils.django.filters.FilterBackend

--- utils/django/filters.py
+++ utils/django/filters.py
@@ -1,4 +1,5 @@
-from django_filters import TypedChoiceFilter
+from django_filters import ChoiceFilter, TypedChoiceFilter
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 
 
 class ChoiceDisplayFilter(TypedChoiceFilter):
@@ -16,3 +17,18 @@ class ChoiceDisplayFilter(TypedChoiceFilter):
 
     def _coerce(self, name):
         return self.choice_names_to_values.get(name)
+
+
+class ExtendedFilterSet(FilterSet):
+    @classmethod
+    def filter_for_lookup(cls, f, lookup_type):
+        filter_class, params = super().filter_for_lookup(f, lookup_type)
+
+        if filter_class is ChoiceFilter:
+            filter_class = ChoiceDisplayFilter
+
+        return filter_class, params
+
+
+class FilterBackend(DjangoFilterBackend):
+    default_filter_set = ExtendedFilterSet
