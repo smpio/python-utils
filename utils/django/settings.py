276754d4b33dcd1d2a2a5acfeb64358ca5fd9910
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-04-25 20:41:02 +0300

    split django.settings.init into multiple functions

--- utils/django/settings.py
+++ utils/django/settings.py
@@ -10,7 +10,36 @@ from utils.log_config import get_logging_config
 def init(settings_module_name, enable_database=True, **env_scheme):
     project_name = settings_module_name.rsplit('.', 1)[0]
     settings = sys.modules[project_name + '.settings']
+    env = get_env(project_name, env_scheme)
 
+    settings.PROJECT_NAME = project_name
+    settings.BUILD_ID = env('BUILD_ID')
+
+    configure_debugging(settings, env)
+    configure_security(settings, env)
+    configure_general(settings, env)
+
+    settings.DATABASES = {}
+    if enable_database:
+        configure_databases(settings, env)
+    else:
+        settings.DATABASES = {}
+
+    configure_caches(settings, env)
+    configure_email(settings, env)
+    configure_i18n(settings, env)
+    configure_logging(settings, env)
+    configure_rest_framework(settings, env)
+    configure_celery(settings, env)
+    configure_smp(settings, env)
+
+    for name in env_scheme.keys():
+        setattr(settings, name, env(name))
+
+    return env, settings
+
+
+def get_env(project_name, scheme):
     env = environ.Env(
         DEV_ENV=(bool, True),
         CONTAINER_ENV=(bool, False),
@@ -27,12 +56,10 @@ def init(settings_module_name, enable_database=True, **env_scheme):
         SENTRY_DSN=(str, None),
         ENVIRONMENT_NAME=str,
         SMP_BASE_URL=(str, 'https://api.smp.io/'),
-        **env_scheme,
+        **scheme,
     )
 
-    dev_env = env('DEV_ENV')
-
-    if dev_env:
+    if env('DEV_ENV'):
         env.scheme['SECRET_KEY'] = (str, 'dev')
         env.scheme['SQL_LOGGING'] = (str, True)
         env.scheme['ENVIRONMENT_NAME'] = (str, 'dev')
@@ -45,37 +72,49 @@ def init(settings_module_name, enable_database=True, **env_scheme):
             for setting_name in ('CACHE_URL', 'CELERY_BROKER_URL', 'CELERY_RESULT_BACKEND_URL'):
                 env.scheme[setting_name] = (str, env.scheme[setting_name][1].replace('//redis', '//localhost'))
 
-    # Debugging
-    settings.DEBUG = dev_env
+    return env
 
-    # Security
+
+def configure_debugging(settings, env):
+    settings.DEBUG = env('DEV_ENV')
+
+
+def configure_security(settings, env):
     settings.SECRET_KEY = env('SECRET_KEY')
     settings.ALLOWED_HOSTS = ['*']
     settings.SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 
-    # General
+
+def configure_general(settings, env):
     settings.INSTALLED_APPS = [
-        project_name + '.App',
+        settings.PROJECT_NAME + '.App',
     ]
+
     settings.MIDDLEWARE = [
     ]
+
     settings.APPEND_SLASH = False
-    settings.ROOT_URLCONF = project_name + '.urls'
-    settings.WSGI_APPLICATION = project_name + '.wsgi.application'
+    settings.ROOT_URLCONF = settings.PROJECT_NAME + '.urls'
+    settings.WSGI_APPLICATION = settings.PROJECT_NAME + '.wsgi.application'
 
-    # Databases
+
+def configure_databases(settings, env):
     settings.DATABASES = {}
-    if enable_database:
-        if env('DATABASE_URL'):
-            settings.DATABASES['default'] = env.db_url('DATABASE_URL')
-        if env('RO_DATABASE_URL'):
-            settings.DATABASES['readonly'] = env.db_url('RO_DATABASE_URL')
-            settings.DATABASES['readonly']['TEST'] = {
-                'MIRROR': 'default',
-            }
-        for db in settings.DATABASES.values():
-            db['ATOMIC_REQUESTS'] = True
 
+    if env('DATABASE_URL'):
+        settings.DATABASES['default'] = env.db_url('DATABASE_URL')
+
+    if env('RO_DATABASE_URL'):
+        settings.DATABASES['readonly'] = env.db_url('RO_DATABASE_URL')
+        settings.DATABASES['readonly']['TEST'] = {
+            'MIRROR': 'default',
+        }
+
+    for db in settings.DATABASES.values():
+        db['ATOMIC_REQUESTS'] = True
+
+
+def configure_caches(settings, env):
     # Caches
     settings.CACHES = {
         'locmem': {
@@ -85,34 +124,26 @@ def init(settings_module_name, enable_database=True, **env_scheme):
     if env('CACHE_URL'):
         settings.CACHES['default'] = env.cache_url('CACHE_URL')
 
-    # Email
+
+def configure_email(settings, env):
     if env('EMAIL_URL'):
         settings.EMAIL_CONFIG = env.email_url('EMAIL_URL')
+
     settings.DEFAULT_FROM_EMAIL = 'SMP.io <noreply@smp.io>'
 
-    # Internationalization
+
+def configure_i18n(settings, env):
     settings.TIME_ZONE = 'UTC'
     settings.USE_I18N = False
     settings.USE_L10N = False
     settings.USE_TZ = False
 
-    # Logging
+
+def configure_logging(settings, env):
     settings.LOGGING = get_logging_config(provider=env('LOGGING'),
                                           log_sql=env('SQL_LOGGING'),
                                           enable_sentry=bool(env('SENTRY_DSN')))
 
-    # REST Framework
-    settings.REST_FRAMEWORK = {
-        'DEFAULT_RENDERER_CLASSES': (
-            'rest_framework.renderers.JSONRenderer',
-        ),
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    }
-
-    # Site wide
-    settings.BUILD_ID = env('BUILD_ID')
-
-    # Raven
     if env('SENTRY_DSN'):
         settings.RAVEN_CONFIG = {
             'dsn': env('SENTRY_DSN'),
@@ -126,12 +157,21 @@ def init(settings_module_name, enable_database=True, **env_scheme):
             'environment': env('ENVIRONMENT_NAME'),
         }
 
-    # DRF Swagger
+
+def configure_rest_framework(settings, env):
+    settings.REST_FRAMEWORK = {
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+        ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    }
+
     settings.SWAGGER_SETTINGS = {
         'USE_SESSION_AUTH': False,
     }
 
-    # Celery
+
+def configure_celery(settings, env):
     if env('CELERY_BROKER_URL'):
         settings.CELERY_BROKER_URL = env('CELERY_BROKER_URL')
     if env('CELERY_RESULT_BACKEND_URL'):
@@ -147,17 +187,9 @@ def init(settings_module_name, enable_database=True, **env_scheme):
     settings.CELERY_TASK_DEFAULT_QUEUE = 'default'
     settings.CELERY_TASK_CREATE_MISSING_QUEUES = False
 
-    # SMP
+
+def configure_smp(settings, env):
     smp.SmpApiClient.base_url = env('SMP_BASE_URL')
-    if dev_env:
+    if env('DEV_ENV'):
         smp.SmpApiClient.default_timeout = None
         smp.SmpApiClient.max_tries = 1
-
-    # Custom
-    settings.PROJECT_NAME = project_name
-
-    # Project custom
-    for name in env_scheme.keys():
-        setattr(settings, name, env(name))
-
-    return env, settings
