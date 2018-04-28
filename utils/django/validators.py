6dd1bf72ec8f83e0c8cabe6985595b6d69fcdafb
Nikita Kurinnyi <f1aky927@gmail.com>
2018-04-28 14:01:52 +0300

    added InValidor for drf serializers

--- utils/django/validators.py
+++ utils/django/validators.py
@@ -8,3 +8,12 @@ class EqualsValidator:
     def __call__(self, value):
         if value != self.value:
             raise ValidationError(f'This field must equal to {repr(self.value)}.')
+
+
+class InValidator:
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self, value):
+        if value not in self.value:
+            raise ValidationError(f'This field must be in {repr(self.value)}.')
