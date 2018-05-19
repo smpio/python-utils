809008d4441d44236ca315d239c25d98d5acb782
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-05-19 03:04:42 +0300

    fix utils.django.renderers.JSONRenderer for 204

--- utils/django/renderers.py
+++ utils/django/renderers.py
@@ -1,11 +1,15 @@
+from rest_framework import status
 from rest_framework.renderers import JSONRenderer as DRFJSONRenderer
 
 
 class JSONRenderer(DRFJSONRenderer):
-
     def render(self, data, accepted_media_type=None, renderer_context=None):
         ret = super().render(data, accepted_media_type, renderer_context)
-        if ret == b'':
+        if ret == b'' and not self.is_no_content(renderer_context):
             ret = b'null'
 
         return ret
+
+    @staticmethod
+    def is_no_content(renderer_context):
+        return renderer_context is not None and renderer_context['response'].status_code == status.HTTP_204_NO_CONTENT
