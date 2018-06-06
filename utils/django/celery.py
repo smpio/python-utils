6ce0b07c1d639d3a8243a63e02ff119b93edd57c
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-06-07 02:55:39 +0300

    fix utils.django.PostTransactionMixin

--- utils/django/celery.py
+++ utils/django/celery.py
@@ -6,10 +6,12 @@ class PostTransactionMixin:
     using = DEFAULT_DB_ALIAS
 
     def apply_async(self, *args, **kwargs):
+        original_apply_async = super().apply_async
+
         if self.using is not None:
             celery_eager = self.app.conf.task_always_eager
             connection = transaction.get_connection(using=self.using)
             if connection.in_atomic_block and not celery_eager:
-                return transaction.on_commit(lambda: super().apply_async(*args, **kwargs), using=self.using)
+                return transaction.on_commit(lambda: original_apply_async(*args, **kwargs), using=self.using)
 
-        return super().apply_async(*args, **kwargs)
+        return original_apply_async(*args, **kwargs)
