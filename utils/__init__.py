095a65d098ea36cba0ee0dbcaa26a9b022b3f963
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-06-07 00:54:52 +0300

    add retry_on_exception decorator

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,8 @@
 import sys
+import time
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def replace_module(module_name, obj):
@@ -30,4 +34,24 @@ def attrs(**kwargs):
         for k, v in kwargs.items():
             setattr(obj, k, v)
         return obj
+    return decorator
+
+
+def retry_on_exception(exception_classes=Exception, max_tries=3, retry_backoff_factor=0.5):
+    def decorator(func):
+        def wrapped(*args, **kwargs):
+            errors = []
+            backoff_time = retry_backoff_factor
+            for try_idx in range(max_tries):
+                if try_idx > 0:
+                    time.sleep(backoff_time)
+                    backoff_time *= 2
+                try:
+                    return func(*args, **kwargs)
+                except exception_classes as e:
+                    log.warning('%s failed', func, exc_info=True)
+                    errors.append(e)
+            raise errors[-1]
+
+        return wrapped
     return decorator
