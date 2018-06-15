4724f8031d15078e5f88423dc7f94b28cd68f249
Nikita Kurinnyi <f1aky927@gmail.com>
2018-06-15 18:21:20 +0300

    added is_url method

--- utils/url.py
+++ utils/url.py
@@ -1,5 +1,13 @@
 import urllib.parse
 
 
+def is_url(url):
+    try:
+        result = urllib.parse.urlparse(url)
+        return all([result.scheme, result.netloc, result.path])
+    except Exception:
+        return False
+
+
 def is_absolute(url):
     return bool(urllib.parse.urlsplit(url).netloc)
