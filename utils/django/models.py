ef17e4d2c3b24cd1b428e5490543a44c2ed49d01
Dmitriy Don <dmitriysdon@gmail.com>
2018-07-10 02:31:25 +0300

    defaults-django: sugar for EnumField

--- utils/django/models.py
+++ utils/django/models.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
+from enum import EnumMeta
 from django.db import models
 
 
@@ -8,4 +9,18 @@ class EnumField(models.SmallIntegerField):
     Special class required to determine obviously when filter by name required
     and make FilterSet simpler because it allows do predefine required filter_class
     """
-    pass
+    def __init__(self, *args, **kwargs):
+        if 'choices' in kwargs:
+            raise Exception('This class supposed to set choices automatically from given choices_enum argument')
+
+        choices_enum = kwargs.pop('choices_enum')
+        if not isinstance(choices_enum, EnumMeta):
+            raise TypeError(f'Excpected {EnumMeta.__name__}, got {type(choices_enum).__name__}')
+        self.choices_enum = choices_enum
+
+        if hasattr(self.choices_enum, 'as_choices'):
+            choices = self.choices_enum.as_choices()
+        else:
+            choices = [(attr.value, attr.name) for attr in self.choices_enum]
+        kwargs['choices'] = choices
+        super().__init__(*args, **kwargs)
