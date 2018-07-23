3a376102ad3c0b25d6533de4ca5e8d6879e2585d
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-07-24 00:54:19 +0300

    add django.celery.AtomicMixin

--- utils/django/celery.py
+++ utils/django/celery.py
@@ -15,3 +15,19 @@ class PostTransactionMixin:
                 return transaction.on_commit(lambda: original_apply_async(*args, **kwargs), using=self.using)
 
         return original_apply_async(*args, **kwargs)
+
+
+class AtomicMixin:
+    using = DEFAULT_DB_ALIAS
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not hasattr(self, 'atomic'):
+            self.atomic = False
+
+    def __call__(self, *args, **kwargs):
+        if self.atomic:
+            with transaction.atomic(using=self.using):
+                return super().__call__(*args, **kwargs)
+        else:
+            return super().__call__(*args, **kwargs)
