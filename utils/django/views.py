023b35a54027aa043885ecaef6eade9d1799b39a
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-07-31 17:20:34 +0300

    utils.django.views.get_open_api_view: add support for 'application/json' Accept header

--- utils/django/views.py
+++ utils/django/views.py
@@ -8,7 +8,10 @@ def get_open_api_view(*args, **kwargs):
     class Renderer(OpenAPIRenderer):
         extra = kwargs.pop('extra', None)
 
-    kwargs.setdefault('renderer_classes', [Renderer])
+    class JsonRenderer(Renderer):
+        media_type = 'application/json'
+
+    kwargs.setdefault('renderer_classes', [Renderer, JsonRenderer])
     kwargs.setdefault('generator_class', SchemaGenerator)
     return schemas.get_schema_view(*args, **kwargs)
 
