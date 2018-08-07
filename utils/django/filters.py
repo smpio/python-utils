00f42ef383f7a9b4a500cfb1ce073109a1ea7f2d
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-08-07 19:18:39 +0300

    add utils.django.filters.ArrayFilter

--- utils/django/filters.py
+++ utils/django/filters.py
@@ -1,11 +1,12 @@
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import JSONField, ArrayField
 from django_filters.filters import EMPTY_VALUES
 from django_filters.rest_framework import *  # noqa
-from django_filters.rest_framework import TypedChoiceFilter, FilterSet, ChoiceFilter, DjangoFilterBackend
+from django_filters.rest_framework import FilterSet as BaseFilterSet
+from django_filters.rest_framework import TypedChoiceFilter, ChoiceFilter, DjangoFilterBackend
 
 from rest_framework.filters import OrderingFilter as DRFOrderingFilter
 
-from .forms.fields import JsonField
+from .forms.fields import JsonField as JsonFormField
 from .models import EnumField
 
 
@@ -27,7 +28,7 @@ class ChoiceDisplayFilter(TypedChoiceFilter):
 
 
 class JsonFilter(Filter):
-    field_class = JsonField
+    field_class = JsonFormField
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('lookup_expr', 'exact')
@@ -47,11 +48,33 @@ class JsonFilter(Filter):
         return qs
 
 
-class FilterSet(FilterSet):
-    FILTER_DEFAULTS = dict(FilterSet.FILTER_DEFAULTS)
+class ArrayFilter(BaseCSVFilter):
+    def filter(self, qs, value):
+        if value in EMPTY_VALUES:
+            return qs
+        if self.distinct:
+            qs = qs.distinct()
+
+        method = self.get_method(qs)
+        if self.lookup_expr == 'identical':
+            qs = method(**{
+                f'{self.field_name}__contains': value,
+                f'{self.field_name}__contained_by': value,
+            })
+        else:
+            qs = method(**{
+                f'{self.field_name}__{self.lookup_expr}': value,
+            })
+
+        return qs
+
+
+class FilterSet(BaseFilterSet):
+    FILTER_DEFAULTS = dict(BaseFilterSet.FILTER_DEFAULTS)
     FILTER_DEFAULTS.update({
         JSONField: {'filter_class': JsonFilter},
         EnumField: {'filter_class': ChoiceDisplayFilter},
+        ArrayField: {'filter_class': ArrayFilter},
     })
 
     @classmethod
@@ -72,6 +95,5 @@ class FilterBackend(DjangoFilterBackend):
 
 
 class OrderingFilter(DRFOrderingFilter):
-
     def get_default_ordering(self, view):
         return super().get_default_ordering(view=view) or ('-pk',)
