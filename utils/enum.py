e0851c4109d70229f93340f1632ca98cb01834a3
Adrian Tangochin <tangochin@yandex.ru>
2019-03-18 19:17:09 +0500

    Revert custom enum class

--- utils/enum.py
+++ utils/enum.py
@@ -1,25 +1,7 @@
 import enum
 
-from django.utils.encoding import force_text
 
-
-class Enum(enum.Enum):
-    @classmethod
-    def choices(cls):
-        """
-        Returns a list formatted for use as field choices.
-        (See https://docs.djangoproject.com/en/dev/ref/models/fields/#choices)
-        """
-        return tuple((m.value, m.name) for m in cls)
-
-    def __str__(self):
-        """
-        Show our label when Django uses the Enum for displaying in a view
-        """
-        return force_text(self.name)
-
-
-class AutoNameEnum(Enum):
+class AutoNameEnum(enum.Enum):
     def _generate_next_value_(name, start, count, last_values):
         return name
 
@@ -46,8 +28,3 @@ class CaseInsensitiveAutoNameEnum(AutoNameEnum):
         if isinstance(other, str):
             other = other.lower()
         return super().__eq__(other)
-
-
-class IntEnum(int, Enum):
-    def __str__(self):
-        return force_text(self.name)
