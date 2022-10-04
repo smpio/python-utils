799d6f24484ae630ac2258dca89203f970e733d7
Dmitrii Don <dmitriysdon@gmail.com>
2022-10-04 13:11:43 +0300

    rollback to Django 3.2 for apps which is not updated yet

--- utils/django/__init__.py
+++ utils/django/__init__.py
@@ -1,8 +1,7 @@
 import django
 from django.db import connection
 
-assert django.VERSION[:2] in ((4, 0), (4, 1)), \
-    f'Incompatible Django version: 4.0.* or 4.1.* expected, got {".".join(map(str, django.VERSION))}'
+assert django.VERSION[:2] == (3, 2), 'Incompatible Django version'
 
 
 def estimate_count(model_class):
