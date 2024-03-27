855166b6cc74e1493c7eabcc5ae97064315f424a
konstantinprilepin <deftonee@ya.ru>
2024-03-27 21:38:38 +0300

    fixed get_redis_connection for django-redis backend

--- utils/redis.py
+++ utils/redis.py
@@ -6,6 +6,9 @@ def get_redis_connection() -> Redis:
     try:
         client = django_cache._cache.get_client(None, write=True)
     except (AttributeError, IndexError, KeyError) as e:
-        raise ValueError('Unable to get Redis connection') from e
+        try:
+            client = django_cache.client.get_client()
+        except (AttributeError, IndexError, KeyError) as e:
+            raise ValueError('Unable to get Redis connection') from e
     assert isinstance(client, Redis), 'Redis is expected as default cache backend'
     return client
